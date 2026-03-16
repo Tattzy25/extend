3eee461a66922da79276b0530434e8e9c3c2b3ad
tattzy25 <tattzy2025@gmail.com>
2026-03-16 12:31:49 -0700

    lets see

--- lib/models.ts
+++
@@ -1,6 +0,0 @@
-export const AVAILABLE_MODELS = [
-  {
-    id: "tattzy25/famous-flux:9d51097c0ad12337cd012d7796e61fe20fdcfe42f03fcca4546eb36b62636962",
-    description: "Custom Flux 1 Dev LoRA for portrait/automotive fusion."
-  }
-]
